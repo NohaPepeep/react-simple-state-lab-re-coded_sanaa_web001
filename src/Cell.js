ac925c25ecedc0bdbf5a76865005732c6c2c245b
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-10-03 01:12:21 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -4,23 +4,22 @@ export default class Cell extends Component {
   constructor(props) {
   super()
   this.state = {
-    color : props.value,
+    color:props.value,
   } // ...define initial state with a key of 'color' set to the 'value' prop
 }
 
-handleClick = (event) =>{ 
-  this.setState({
+handleClick = (event) =>{
+  this.setState(state => ({
       color:"#333"
-    });
+    }));
 }
-  
+
   render() {
     return (
-      <div className="cell" id="ev" style={{backgroundColor: `${this.state.color}`}} onClick={this.handleClick}>
-        
+      <div className="cell" id="ev" onClick={this.state.color == "#333"? :this.handleClick} style={{backgroundColor:this.state.color}} >
       </div>
     )
   }
-  
-}
 
+}
+ 
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Cell from './Cell.js'
 
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-    return vals.map(val => <Cell value={val} />) // replace me and render a cell component instead!
+    return vals.map(val => <Cell value={val} />)
   }
   
   genMatrix = () => {
@@ -19,7 +20,6 @@ export default class Matrix extends Component {
   }
   
 }
-
 Matrix.defaultProps = {
   values:[['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
